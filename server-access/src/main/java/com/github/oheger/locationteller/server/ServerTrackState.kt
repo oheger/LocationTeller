7cdd4ed33261e2fdcbc93fb435296227d2f6f7a4
oheger <oliver.heger@oliver-heger.de>
2019-06-20 18:56:22 +0200

    Added a class to represent the tracking state on the server.
    The class contains information about the files with location data that
    are currently available on the server.

---
+++ server-access/src/main/java/com/github/oheger/locationteller/server/ServerTrackState.kt
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2019 The Developers.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.oheger.locationteller.server
+
+/**
+ * A data class describing a file reference on the server with location
+ * information.
+ *
+ * Such files are organized in folders (below the base path of a track
+ * configuration). Based on naming conventions, the time when the location was
+ * recorded can be determined. This class contains the components to identify
+ * a file. Note that not the actual names are stored, but only the portions to
+ * determine the time of the data.
+ */
+data class FileData(val folderRef: String, val fileRef: String)
+
+/**
+ * A data class representing the location-related information stored on the
+ * server.
+ *
+ * An instance of this class can be used to find out what location data and -
+ * based on file naming conventions - from when is currently stored on the
+ * server. This is needed to display this information and to update the server
+ * accordingly, e.g. to remove outdated location data.
+ *
+ * Note that the class does not store location data directly, but only
+ * references to files stored on the server. This simplifies certain update
+ * operations.
+ *
+ * @param files a list with the data of files with location data
+ */
+data class ServerTrackState(val files: List<FileData>) {
+    /**
+     * Returns a _Pair_ with files that are outdated according to the given
+     * reference time and an updated _ServerTrackState_ object that has these
+     * files removed. This function can be used to find the files that need to
+     * be removed from the server and in parallel generate an updated state
+     * object.
+     * @param refTime the reference time; older files are removed
+     * @return a _Pair_ with outdated files and an updated state
+     */
+    fun removeOutdatedFiles(refTime: TimeData): Pair<List<FileData>, ServerTrackState> {
+        val partition = files.partition { data ->
+            data.folderRef < refTime.dateString ||
+                    (data.folderRef == refTime.dateString && data.fileRef < refTime.timeString)
+        }
+
+        return if (partition.first.isEmpty()) Pair(emptyList(), this)
+        else Pair(partition.first, ServerTrackState(partition.second))
+    }
+
+    /**
+     * Returns an updated state object does not contain any of the specified
+     * files.
+     * @param removeFiles a set with the files to be removed
+     * @return the updated state object
+     */
+    fun removeFiles(removeFiles: Set<FileData>): ServerTrackState {
+        val nextFiles = files.filter { !removeFiles.contains(it) }
+        return ServerTrackState(nextFiles)
+    }
+
+    /**
+     * Returns an updated state object that contains the given _FileData_
+     * object at the last position. This function expects that the sort order
+     * is correctly maintained by appending this file.
+     * @param file the file to be appended
+     * @return the updated state object
+     */
+    fun appendFile(file: FileData): ServerTrackState {
+        val newFiles = ArrayList<FileData>(files.size + 1)
+        newFiles.addAll(files)
+        newFiles.add(file)
+        return ServerTrackState(newFiles)
+    }
+
+    /**
+     * Checks whether a folder with the given reference is contained in this
+     * state object.
+     * @param ref the reference pointing to the folder
+     * @return *true* if such a folder is present; *false* otherwise
+     */
+    fun hasFolder(ref: String): Boolean {
+        val filteredFiles = files.dropWhile { it.folderRef < ref }
+        return if (filteredFiles.isEmpty()) false
+        else filteredFiles.first().folderRef == ref
+    }
+}
---
+++ server-access/src/test/java/com/github/oheger/locationteller/server/ServerTrackStateSpec.kt
@@ -0,0 +1,173 @@
+/*
+ * Copyright 2019 The Developers.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.oheger.locationteller.server
+
+import io.kotlintest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotlintest.matchers.collections.shouldHaveSize
+import io.kotlintest.matchers.types.shouldBeSameInstanceAs
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+import java.util.*
+
+/**
+ * Test class for [ServerTrackState].
+ */
+class ServerTrackStateSpec : StringSpec() {
+    /**
+     * Creates a _TimeData_ object representing a specific point in time.
+     * @param day the day (year and month are fix)
+     * @param hour the hour
+     * @param min the minute
+     * @param sec the seconds
+     */
+    private fun time(day: Int, hour: Int, min: Int, sec: Int): TimeData {
+        val cal = Calendar.getInstance()
+        cal.clear()
+        cal.set(2019, Calendar.JUNE, day, hour, min, sec)
+        return TimeData(cal.timeInMillis)
+    }
+
+    /**
+     * Returns a _FileData_ object based on the naming conventions to represent
+     * the given time.
+     * @param time the time of the file
+     * @return the corresponding _FileData_
+     */
+    private fun fileDataForTime(time: TimeData): FileData =
+        FileData(time.dateString, time.timeString)
+
+    init {
+        "ServerTrackState should handle an empty state when finding outdated files" {
+            val state = ServerTrackState(emptyList())
+
+            val result = state.removeOutdatedFiles(time(20, 18, 13, 20))
+            result.first shouldHaveSize 0
+            result.second shouldBeSameInstanceAs state
+        }
+
+        "ServerTrackState should find outdated files" {
+            val times = listOf(
+                time(18, 8, 20, 17),
+                time(18, 10, 30, 27),
+                time(19, 8, 11, 12),
+                time(19, 10, 20, 30),
+                time(19, 13, 13, 13),
+                time(19, 17, 28, 55),
+                time(20, 1, 42, 19)
+            )
+            val files = times.map(::fileDataForTime)
+            val refTime = time(19, 12, 0, 0)
+            val outdated = files.subList(0, 4)
+            val remaining = files.subList(4, files.size)
+            val state = ServerTrackState(files)
+
+            val result = state.removeOutdatedFiles(refTime)
+            result.first shouldContainExactlyInAnyOrder outdated
+            result.second.files shouldContainExactlyInAnyOrder remaining
+        }
+
+        "ServerTrackState should handle the case that all files are outdated" {
+            val times = listOf(
+                time(20, 6, 59, 50),
+                time(20, 7, 11, 0),
+                time(20, 11, 59, 59)
+            )
+            val files = times.map(::fileDataForTime)
+            val refTime = time(20, 12, 0, 0)
+            val state = ServerTrackState(files)
+
+            val result = state.removeOutdatedFiles(refTime)
+            result.first shouldContainExactlyInAnyOrder files
+            result.second.files shouldHaveSize 0
+        }
+
+        "ServerTrackState should handle the case that all files are up-to-date" {
+            val times = listOf(
+                time(20, 0, 0, 0),
+                time(20, 12, 0, 0),
+                time(21, 1, 1, 1)
+            )
+            val files = times.map(::fileDataForTime)
+            val refTime = time(19, 23, 59, 59)
+            val state = ServerTrackState(files)
+
+            val result = state.removeOutdatedFiles(refTime)
+            result.first shouldHaveSize 0
+            result.second shouldBeSameInstanceAs state
+        }
+
+        "ServerTrackState should report the presence of a folder if empty" {
+            val state = ServerTrackState(emptyList())
+
+            state.hasFolder("2019-06-22") shouldBe false
+        }
+
+        "ServerTrackState should report the presence of a folder if it is found" {
+            val times = listOf(
+                time(21, 10, 11, 12),
+                time(21, 15, 16, 7),
+                time(22, 8, 9, 10)
+            )
+            val state = ServerTrackState(times.map(::fileDataForTime))
+
+            state.hasFolder("2019-06-22") shouldBe true
+        }
+
+        "ServerTrackState should report the presence of a folder if it is too small" {
+            val times = listOf(
+                time(21, 10, 11, 12),
+                time(21, 15, 16, 7),
+                time(22, 8, 9, 10)
+            )
+            val state = ServerTrackState(times.map(::fileDataForTime))
+
+            state.hasFolder("2019-06-20") shouldBe false
+        }
+
+        "ServerTrackState should report the presence of a folder if it is too big" {
+            val times = listOf(
+                time(21, 10, 11, 12),
+                time(21, 15, 16, 7),
+                time(22, 8, 9, 10)
+            )
+            val state = ServerTrackState(times.map(::fileDataForTime))
+
+            state.hasFolder("2019-06-23") shouldBe false
+        }
+
+        "ServerTrackState should support adding a FileData" {
+            val file1 = fileDataForTime(time(22, 20, 58, 10))
+            val file2 = fileDataForTime(time(22, 20, 59, 20))
+            val fileNew = fileDataForTime(time(22, 21, 0, 30))
+            val state = ServerTrackState(listOf(file1, file2))
+
+            val nextState = state.appendFile(fileNew)
+            nextState.files shouldContainExactlyInAnyOrder listOf(file1, file2, fileNew)
+        }
+
+        "ServerTrackState should support removing files" {
+            val file1 = fileDataForTime(time(22, 20, 58, 10))
+            val file2 = fileDataForTime(time(22, 20, 59, 20))
+            val file3 = fileDataForTime(time(22, 21, 0, 30))
+            val file4 = fileDataForTime(time(22, 21, 9, 24))
+            val state = ServerTrackState(listOf(file1, file2, file3, file4))
+            val removeFiles = setOf(file1, file4, fileDataForTime(time(1, 1, 2, 3)))
+
+            val nextState = state.removeFiles(removeFiles)
+            nextState.files shouldContainExactlyInAnyOrder listOf(file2, file3)
+        }
+    }
+}
