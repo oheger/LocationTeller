7e439cb28c202979d43319252b48e63252028867
oheger <oliver.heger@oliver-heger.de>
2019-06-23 22:11:39 +0200

    Added initial implementation of TrackService.
    This class interacts with the track server using an application-level
    protocol (in contrast to DavClient which provides only low-level
    functionality).
    
    So far only a limited subset of the required functionality has been
    implemented: the content of the server can be queried, and outdated
    location data can be removed.

---
+++ server-access/src/main/java/com/github/oheger/locationteller/server/TrackService.kt
@@ -0,0 +1,235 @@
+/*
+ * Copyright 2019 The Developers.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.oheger.locationteller.server
+
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+
+/**
+ * A class providing functionality to interact with a server to retrieve
+ * tracked locations or add new location data.
+ *
+ * This class implements the major part of the logic for managing location data
+ * on a server. It uses an instance of [DavClient] to access the server and
+ * manipulates files there.
+ *
+ * Location data is stored on the server in files following specific naming
+ * conventions:
+ * * For each day a folder is created whose name is derived from the date.
+ * * Each track folder contains files for the single location updates that have
+ * been posted. The file names are derived from the time associated with the
+ * location.
+ *
+ * An instance of this class manages an object with the state of the server
+ * (i.e. the location data that has been uploaded). This state is accessed and
+ * updated when operations are executed. Note that this class is not
+ * thread-safe! Callers are responsible for a correct synchronization.
+ *
+ * @param timeService the _TimeService_ used by this instance
+ * @param davClient the _DavClient_ used by this instance
+ */
+class TrackService(val timeService: TimeService, val davClient: DavClient) {
+    /**
+     * Stores the tracking state on the server. This field is initialized on
+     * first access.
+     */
+    private var trackState: ServerTrackState? = null
+
+    /**
+     * Returns a list with the paths of the files with location data that are
+     * currently stored on the server.
+     * @return a list with paths of files on the server
+     */
+    suspend fun filesOnServer(): List<String> =
+        filesFromTrackState(getTrackState())
+
+    /**
+     * Determines the files which are outdated according to the given reference
+     * time. These files are removed from the server (and the track state kept
+     * locally is updated).
+     * @param refTime the reference time
+     * @return a flag whether all updates have been successful
+     */
+    suspend fun removeOutdated(refTime: TimeData): Boolean = coroutineScope {
+        val oldState = getTrackState()
+        val (removeFiles, nextState) = oldState.removeOutdatedFiles(refTime)
+        if (removeFiles.isNotEmpty()) {
+            val removePaths = calcRemovePaths(removeFiles, nextState)
+            val removeOpsAsync = removePaths.map { path -> async { davClient.delete(path) } }
+            val removeOps = removeOpsAsync.awaitAll()
+            if (removeOps.all { it }) {
+                trackState = nextState
+                true
+            } else {
+                if (removeOps.any { it }) {
+                    val successFiles = successfullyRemovedFiles(removePaths, removeOps, oldState)
+                    trackState = oldState.removeFiles(successFiles)
+                }
+                false
+            }
+        } else true
+    }
+
+    /**
+     * Returns the current track state. If the data is already available, it is
+     * returned directly. Otherwise, it needs to be fetched from the server
+     * first.
+     * @return the current _ServerTrackState_
+     */
+    private suspend fun getTrackState(): ServerTrackState {
+        val state = trackState
+        if (state != null) return state
+
+        val initState = initTrackState()
+        trackState = initState
+        return initState
+    }
+
+    /**
+     * Loads data from the track server and creates a corresponding
+     * _ServerTrackState_ object from it.
+     * @return the track state
+     */
+    private suspend fun initTrackState(): ServerTrackState = coroutineScope {
+        val rootFolder = davClient.loadFolder("")
+        val trackFoldersAsync = rootFolder.elements
+            .filter { davElement -> davElement.isFolder && davElement.name.startsWith(NamePrefix) }
+            .map { async { davClient.loadFolder("/${it.name}") } }
+        val trackFolders = trackFoldersAsync.awaitAll()
+        val files = trackFolders.flatMap { folder ->
+            folder.elements.filter { davElement -> !davElement.isFolder && davElement.name.startsWith(NamePrefix) }
+                .map { FileData(folder.path.substring(NamePrefix.length + 1), it.name.substring(PrefixLength)) }
+        }
+        ServerTrackState(files)
+    }
+
+    companion object {
+        /** A prefix for the file and folder names with location data.*/
+        const val NamePrefix = "track_"
+
+        /** The length of the prefix used for folders and files.*/
+        private const val PrefixLength = NamePrefix.length
+
+        /** The length of a folder reference (YYYY-MM-DD).*/
+        private const val FolderRefLength = 10
+
+        /** The length of a file reference (HH_MM_SS).*/
+        private const val FileRefLength = 8
+
+        /**
+         * Creates a new, fully initialized instance of _TrackService_. Default
+         * helper objects are created.
+         * @param config the server configuration
+         */
+        fun create(config: ServerConfig): TrackService {
+            val client = DavClient.create(config)
+            return TrackService(CurrentTimeService, client)
+        }
+
+        /**
+         * Extracts the path of the folder from a _FileData_ object.
+         * @return the folder path
+         */
+        private fun FileData.folderPath(): String =
+            "/$NamePrefix$folderRef/"
+
+        /**
+         * Converts a _FileData_ object into a corresponding path.
+         * @return the path
+         */
+        private fun FileData.toPath(): String =
+            "${folderPath()}$NamePrefix$fileRef"
+
+        /**
+         * Generates a list with the files on the server from the given state
+         * object. The paths of the files are generated based on naming
+         * conventions and the date/time information.
+         * @param state the track state object
+         * @return the corresponding list of files
+         */
+        private fun filesFromTrackState(state: ServerTrackState): List<String> =
+            state.files.map { it.toPath() }
+
+        /**
+         * Extracts the references for the folder and the file from the given
+         * path. This is possible because of naming conventions. Note that all
+         * paths have the same length.
+         * @param path the path pointing to a location file
+         * @return the corresponding _FileData_ for this file
+         */
+        private fun fileDataFromPath(path: String): FileData {
+            val folderRef = folderRefFromPath(path)
+            val fileRef = path.substring(path.length - FileRefLength)
+            return FileData(folderRef, fileRef)
+        }
+
+        /**
+         * Extracts the folder reference from a path for a file or folder.
+         * @param path the path
+         * @return the folder reference from the path
+         */
+        private fun folderRefFromPath(path: String) =
+            path.substring(PrefixLength + 1, PrefixLength + 1 + FolderRefLength)
+
+        /**
+         * Determines the paths of files and folders that need to be removed
+         * from the server.
+         * @param removeFiles list of files to be removed
+         * @param nextState the updated tracking state
+         */
+        private fun calcRemovePaths(removeFiles: List<FileData>, nextState: ServerTrackState): MutableSet<String> {
+            val removeFolders = removeFiles.map { it.folderRef }.toSet()
+                .filter { folder -> !nextState.hasFolder(folder) }.toSet()
+            val removePaths = removeFiles
+                .filter { fileData -> !removeFolders.contains(fileData.folderRef) }
+                .map { it.toPath() }.toMutableSet()
+            removePaths.addAll(removeFolders.map { "/$NamePrefix$it/" })
+            return removePaths
+        }
+
+        /**
+         * Determines the files that could be removed successfully in a remove
+         * operation that was only partially successful. In this situation, the
+         * new track state is the old state minus the files that could be
+         * removed successfully.
+         * @param removePaths the set with the paths that should be removed
+         * @param removeOps the boolean results of the single remove operations
+         * @param state the track state object
+         * @return a collection with _FileData_ objects for removed files
+         */
+        private fun successfullyRemovedFiles(
+            removePaths: MutableSet<String>, removeOps: List<Boolean>,
+            state: ServerTrackState
+        ): Set<FileData> {
+            val filePathLength = 2 * PrefixLength + FolderRefLength + FileRefLength
+            val (removeFolders, removeFiles) = removePaths.withIndex()
+                .filter { indexedValue -> removeOps[indexedValue.index] }
+                .partition { it.value.length < filePathLength }
+            val successFiles = removeFiles
+                .map { indexedValue -> fileDataFromPath(indexedValue.value) }
+            val successFolderFiles = removeFolders.flatMap { indexedValue ->
+                val folderRef = folderRefFromPath(indexedValue.value)
+                state.files.filter { it.folderRef == folderRef }
+            }
+
+            val result = mutableSetOf<FileData>()
+            result.addAll(successFiles)
+            result.addAll(successFolderFiles)
+            return result
+        }
+    }
+}
---
+++ server-access/src/test/java/com/github/oheger/locationteller/server/TrackServiceSpec.kt
@@ -0,0 +1,252 @@
+/*
+ * Copyright 2019 The Developers.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.oheger.locationteller.server
+
+import io.kotlintest.matchers.collections.shouldContain
+import io.kotlintest.matchers.collections.shouldNotContain
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+import io.mockk.coEvery
+import io.mockk.mockk
+import java.util.*
+
+/**
+ * Test class for [TrackService].
+ */
+class TrackServiceSpec : StringSpec() {
+    init {
+        "The companion object should create a correct instance" {
+            val config = ServerConfig("someUri", "somePath", "user", "pwd")
+
+            val service = TrackService.create(config)
+            service.davClient.config shouldBe config
+            service.timeService shouldBe CurrentTimeService
+        }
+
+        "TrackService should determine the files on the server" {
+            val davClient = createPreparedDavClient()
+            val service = TrackService(CurrentTimeService, davClient)
+
+            service.filesOnServer() shouldBe expectedFiles
+        }
+
+        "TrackService should remove outdated elements from the server" {
+            val davClient = createPreparedDavClient()
+            val refTime = referenceTime(20, 12, 0, 0)
+            coEvery { davClient.delete(folderPath(folder1)) } returns true
+            coEvery { davClient.delete(expectedFiles[2]) } returns true
+            val service = TrackService(CurrentTimeService, davClient)
+
+            service.removeOutdated(refTime) shouldBe true
+            service.filesOnServer() shouldBe expectedFiles.drop(3)
+        }
+
+        "TrackService should handle the case that no outdated files need to be removed" {
+            val davClient = createPreparedDavClient()
+            val refTime = referenceTime(18, 21, 57, 5)
+            val service = TrackService(CurrentTimeService, davClient)
+            val oldFiles = service.filesOnServer()
+
+            service.removeOutdated(refTime) shouldBe true
+            service.filesOnServer() shouldBe oldFiles
+        }
+
+        "TrackService should remove the last folder if it contains only outdated files" {
+            val davClient = createPreparedDavClient()
+            val refTime = referenceTime(20, 19, 28, 46)
+            coEvery { davClient.delete(folderPath(folder1)) } returns true
+            coEvery { davClient.delete(folderPath(folder2)) } returns true
+            val service = TrackService(CurrentTimeService, davClient)
+
+            service.removeOutdated(refTime) shouldBe true
+        }
+
+        "TrackService should keep the old state if a remove operation fails completely" {
+            val davClient = createPreparedDavClient()
+            val refTime = referenceTime(21, 12, 0, 0)
+            coEvery { davClient.delete(any()) } returns false
+            val service = TrackService(CurrentTimeService, davClient)
+
+            service.removeOutdated(refTime) shouldBe false
+            service.filesOnServer() shouldBe expectedFiles
+        }
+
+        "TrackService should keep files in the state for folders which could not be removed" {
+            val davClient = createPreparedDavClient()
+            val refTime = referenceTime(20, 12, 0, 0)
+            coEvery { davClient.delete(folderPath(folder1)) } returns false
+            coEvery { davClient.delete(expectedFiles[2]) } returns true
+            val service = TrackService(CurrentTimeService, davClient)
+
+            service.removeOutdated(refTime) shouldBe false
+            val files = service.filesOnServer()
+            files shouldContain expectedFiles[0]
+            files shouldContain expectedFiles[1]
+            files shouldNotContain expectedFiles[2]
+        }
+
+        "TrackService should keep single files in the state that could not be removed" {
+            val davClient = createPreparedDavClient()
+            val refTime = referenceTime(20, 12, 0, 0)
+            coEvery { davClient.delete(folderPath(folder1)) } returns true
+            coEvery { davClient.delete(expectedFiles[2]) } returns false
+            val service = TrackService(CurrentTimeService, davClient)
+
+            service.removeOutdated(refTime) shouldBe false
+            val files = service.filesOnServer()
+            files shouldNotContain expectedFiles[0]
+            files shouldNotContain expectedFiles[1]
+            files shouldContain expectedFiles[2]
+        }
+    }
+
+    companion object {
+        /**
+         * Constant for the root folder of the track server with some content.
+         */
+        private val rootFolder = DavFolder(
+            "/", listOf(
+                createElement("2019-06-19", true),
+                createElement("2019-06-20", true),
+                DavElement("otherDir", true),
+                createElement("2019-06-21", true),
+                DavElement("dataFile.xyz", false)
+            )
+        )
+
+        /**
+         * Constant for a test folder with location data of a specific date
+         * with some content.
+         */
+        private val folder1 = DavFolder(
+            "/${elementName("2019-06-19")}",
+            listOf(
+                DavElement("foo.txt", false),
+                createElement("10_05_10", false),
+                DavElement("subFolder", true),
+                createElement("12_18_27", false)
+            )
+        )
+
+        /**
+         * Constant for a test folder with location data of a specific date
+         * with some content.
+         */
+        private val folder2 = DavFolder(
+            "/${elementName("2019-06-20")}",
+            listOf(
+                createElement("11_11_11", false),
+                createElement("19_28_45", false)
+            )
+        )
+
+        /**
+         * Constant for a test folder with location data of a specific date
+         * with some content.
+         */
+        private val folder3 = DavFolder(
+            "/${elementName("2019-06-21")}",
+            listOf(
+                createElement("06_05_58", false),
+                createElement("14_19_33"),
+                createElement("22_02_29", false)
+            )
+        )
+
+        /**
+         * A list with the test files contained on the track server.
+         */
+        val expectedFiles = listOf(
+            filePath(folder1, "10_05_10"),
+            filePath(folder1, "12_18_27"),
+            filePath(folder2, "11_11_11"),
+            filePath(folder2, "19_28_45"),
+            filePath(folder3, "06_05_58"),
+            filePath(folder3, "14_19_33"),
+            filePath(folder3, "22_02_29")
+        )
+
+        /**
+         * Generates the name of a file or folder with location information based
+         * on the given name. Adds the prefix.
+         * @param name the element name
+         * @return the resulting element name
+         */
+        private fun elementName(name: String): String =
+            TrackService.NamePrefix + name
+
+        /**
+         * Generates the path of a folder with a trailing slash.
+         * @param folder the folder
+         * @return the path of this folder
+         */
+        private fun folderPath(folder: DavFolder): String = "${folder.path}/"
+
+        /**
+         * Generates the path to a location file.
+         * @param folder the folder the file is located in
+         * @param fileName the name of the file (without prefix)
+         * @return the resulting file name
+         */
+        private fun filePath(folder: DavFolder, fileName: String): String =
+            "${folderPath(folder)}${elementName(fileName)}"
+
+        /**
+         * Creates an element with tracking data based on the given information.
+         * @param name the element name
+         * @param isFolder flag whether the element is a folder
+         * @return the newly created element
+         */
+        private fun createElement(name: String, isFolder: Boolean = false): DavElement =
+            DavElement(elementName(name), isFolder)
+
+        /**
+         * Returns a _TimeData_ object that is initialized with the given time.
+         * @param date the date (year and month are set to defaults)
+         * @param hour the hour
+         * @param min the minute
+         * @param sec the seconds
+         * @return the _TimeData_ object
+         */
+        private fun referenceTime(date: Int, hour: Int, min: Int, sec: Int): TimeData {
+            val cal = Calendar.getInstance()
+            cal.clear()
+            cal.set(2019, Calendar.JUNE, date, hour, min, sec)
+            return TimeData(cal.timeInMillis)
+        }
+
+        /**
+         * Prepares expectations for the given client mock to return test data
+         * when folders on the server are requested.
+         * @param davClient the mock for the client
+         * @return the same client mock
+         */
+        private fun prepareDavClientForFolders(davClient: DavClient): DavClient {
+            coEvery { davClient.loadFolder("") } returns rootFolder
+            coEvery { davClient.loadFolder(folder1.path) } returns folder1
+            coEvery { davClient.loadFolder(folder2.path) } returns folder2
+            coEvery { davClient.loadFolder(folder3.path) } returns folder3
+            return davClient
+        }
+
+        /**
+         * Creates a mock for a _DavClient_ that is prepared for folder
+         * requests.
+         * @return the prepared mock client
+         */
+        private fun createPreparedDavClient(): DavClient = prepareDavClientForFolders(mockk())
+    }
+}
