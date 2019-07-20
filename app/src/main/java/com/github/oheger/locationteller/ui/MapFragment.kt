f58a181940acf90a3a65d2ebab7a9f015afd0bd1
oheger <oliver.heger@oliver-heger.de>
2019-07-20 22:31:50 +0200

    Added very basic fragment to display a map.
    This is a first experiment to display a map with markers for the
    location files loaded from the server. This is a starting point that
    has to be enhanced with further features.

---
+++ app/src/main/java/com/github/oheger/locationteller/ui/MapFragment.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2019 The Developers.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.oheger.locationteller.ui
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.github.oheger.locationteller.R
+import com.github.oheger.locationteller.map.LocationFileState
+import com.github.oheger.locationteller.map.MapUpdater
+import com.github.oheger.locationteller.track.PreferencesHandler
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+
+class MapFragment : androidx.fragment.app.Fragment(), OnMapReadyCallback, CoroutineScope {
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.IO
+
+    private val logTag = "MapFragment"
+
+    private var map: GoogleMap? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_map, container, false)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.googleMap) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+        return view
+    }
+
+    override fun onMapReady(map: GoogleMap?) {
+        Log.i(logTag, "Map is ready")
+        val prefHandler = PreferencesHandler.create(context!!)
+        val config = prefHandler.createServerConfig()
+        if (config != null && map != null) {
+            launch { MapUpdater.updateMap(config, map, LocationFileState(emptyList(), emptyMap())) }
+        }
+    }
+}
