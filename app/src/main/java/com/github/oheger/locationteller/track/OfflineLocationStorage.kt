5536de8642b005a865531b55df012cb6fa089bfa
oheger <oliver.heger@oliver-heger.de>
2020-02-11 21:57:32 +0100

    Added OfflineLocationStorage class.
    The class is used to buffer location data temporarily that cannot be
    uploaded to the server, e.g. because the mobile connection is
    currently not available. It contains functionality to upload this data
    at a later point in time.

---
+++ app/src/main/java/com/github/oheger/locationteller/track/OfflineLocationStorage.kt
@@ -0,0 +1,189 @@
+/*
+ * Copyright 2019-2020 The Developers.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.oheger.locationteller.track
+
+import com.github.oheger.locationteller.server.LocationData
+import java.util.*
+import kotlin.collections.ArrayList
+
+/**
+ * A class allowing to store location data offline if no internet connection
+ * for the upload is available.
+ *
+ * This class plays a central role in the handling of a broken internet
+ * connection. The basic idea is as follows:
+ *
+ * If the upload of a location data file to the server fails, the data is
+ * passed to an instance of this class. The class stores a configurable number
+ * of records, discarding old ones when the capacity limit is reached. During a
+ * location check, it is tested whether the storage contains any data. If it is
+ * empty, it is assumed that the internet connection is working, and the new
+ * location data is uploaded directly. If there is data in the store, however,
+ * a chunk of data (starting from the most recent ones) is tried to be
+ * uploaded. This is continued with further chunks until an error occurs or a
+ * configurable time limit is reached. The data remaining will then be
+ * processed during the next check.
+ *
+ * This class provides an API to manage the data that could not be uploaded
+ * directly. It takes care that the newest records available take part in
+ * upload operations. It also implements some filtering on the data it needs to
+ * store: If an error during upload is detected, another check is scheduled
+ * after a (typically shorter) retry interval. So more location data would be
+ * recorded. Here the class makes sure that only a single data item (always the
+ * newest one) is kept in the configured minimum tracking interval.
+ *
+ * Implementation note: This class is not thread-safe. It is expected to be
+ * used inside an actor or a corresponding synchronized structure.
+ *
+ * @param capacity the capacity (the max number of items to store)
+ * @param minTrackInterval the minimum track interval (in milliseconds)
+ */
+class OfflineLocationStorage(
+    val capacity: Int,
+    val minTrackInterval: Long
+) {
+    /** Stores the data items in descending order by their creation time. */
+    private val storage = LinkedList<LocationData>()
+
+    /**
+     * Returns a flag whether the given location data object can be uploaded
+     * directly to the server. This is *true* if the storage does not contain
+     * any data, so no buffering is required. Otherwise, the data is processed,
+     * and the storage is updated accordingly.
+     * @param locationData the current _LocationData_
+     * @return a flag whether a direct upload is possible
+     */
+    fun canUploadDirectly(locationData: LocationData): Boolean =
+        if (storage.isEmpty()) true
+        else {
+            addToStorage(locationData)
+            false
+        }
+
+    /**
+     * Passes a _LocationData_ object to this storage that could not be
+     * uploaded. It is added to this storage; this may cause some other data to
+     * be removed.
+     * @param locationData the data object that could not be uploaded
+     */
+    fun storeFailedUpload(locationData: LocationData) {
+        addToStorage(locationData)
+    }
+
+    /**
+     * Requests a chunk of data to be uploaded. This function is called if no
+     * direct upload is possible (because uploads had failed in the past). It
+     * returns an ordered list with the most recent _LocationData_ objects
+     * available (the first item in the list is the newest one) that has at
+     * most the given chunk size. The elements in the list are also removed
+     * from the storage.
+     * @param chunkSize a hint for the chunk size
+     * @return the next chunk of data to be uploaded
+     */
+    fun nextUploadChunk(chunkSize: Int): List<LocationData> {
+        val result = ArrayList<LocationData>(chunkSize)
+        while (!storage.isEmpty() && result.size < chunkSize) {
+            result += storage.removeFirst()
+        }
+        return result
+    }
+
+    /**
+     * Handles the result of an upload operation with multiple elements. If
+     * there were failures, the failed elements need to be added again to the
+     * storage (at the front). The return value indicates whether further
+     * uploads can be done . This is the case if the operation was successful,
+     * and more data is available to be uploaded. The return value is actually
+     * an enumeration constant that carries some more information about the
+     * current status of the storage.
+     * @param chunk the list with data that has been uploaded
+     * @param successCount the number of successful uploads
+     * @return a constant describing the status of the offline storage
+     */
+    fun handleMultiUploadResult(chunk: List<LocationData>, successCount: Int): MultiUploadProgress {
+        if (successCount < chunk.size) {
+            storage.addAll(0, chunk.drop(successCount))
+            return MultiUploadProgress.ERROR
+        }
+
+        return if(storage.isEmpty()) MultiUploadProgress.DONE else MultiUploadProgress.PROGRESS
+    }
+
+    /**
+     * Adds the given (newest) _LocationData_ object to the managed storage.
+     * Also takes care that the maximum capacity is not exceeded by removing
+     * the oldest element if necessary.
+     * @param locationData the data to be added
+     */
+    private fun addToStorage(locationData: LocationData) {
+        if (intervalTooShort(locationData)) {
+            storage.removeFirst()
+        }
+
+        storage.add(0, locationData)
+        if (storage.size > capacity) {
+            storage.removeLast()
+        }
+    }
+
+    /**
+     * Checks the interval in which data elements come in and filters out
+     * elements below the threshold. This function checks if the interval
+     * between the newest element and the second last one that has been added
+     * (if existing) is below the minimum track interval. If so, the element
+     * in between can be removed.
+     * @param locationData a new location data object to be added
+     * @return *true* if an element can be filtered out; else *false*
+     */
+    private fun intervalTooShort(locationData: LocationData) =
+        storage.size > 2 && locationData.time.currentTime - storage[1].time.currentTime < minTrackInterval
+
+    companion object {
+        /**
+         * An enum class defining the possible proceedings after a multi-upload
+         * has been performed.
+         *
+         * Depending on the result of the last upload operation and the
+         * availability of more data to upload, the sync operation can be
+         * continued or aborted. Whether there was an error or not is also an
+         * important information as it impacts the delay until the next
+         * location update is attempted.
+         *
+         * @param canContinue flag whether more data can be uploaded
+         * @param isError flag whether an error occurred
+         */
+        enum class MultiUploadProgress(val canContinue: Boolean, val isError: Boolean) {
+            /**
+             * Constant defining the state that no more data is available for
+             * upload. So the offline storage has been synced successfully.
+             */
+            DONE(canContinue = false, isError = false),
+
+            /**
+             * Constant defining the state that there is more data to be
+             * uploaded. So the sync operation can go on, provided that the
+             * sync time has not yet been exceeded.
+             */
+            PROGRESS(canContinue = true, isError = false),
+
+            /**
+             * Constant defining the state that an upload failed. Then no
+             * further uploads should be tried in this cycle.
+             */
+            ERROR(canContinue = false, isError = true)
+        }
+    }
+}
---
+++ app/src/test/java/com/github/oheger/locationteller/track/OfflineLocationStorageSpec.kt
@@ -0,0 +1,201 @@
+/*
+ * Copyright 2019-2020 The Developers.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.oheger.locationteller.track
+
+import com.github.oheger.locationteller.server.LocationData
+import com.github.oheger.locationteller.server.TimeData
+import io.kotlintest.matchers.collections.shouldContainExactly
+import io.kotlintest.matchers.collections.shouldHaveSize
+import io.kotlintest.matchers.collections.shouldNotContain
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+
+/**
+ * Test class for [[OfflineLocationStorage]].
+ */
+class OfflineLocationStorageSpec : StringSpec() {
+    init {
+        "OfflineLocationStorage should allow a direct upload if the storage is empty" {
+            val storage = createStorage()
+
+            storage.canUploadDirectly(locData(1, 0)) shouldBe true
+        }
+
+        "OfflineLocationStorage should return an empty chunk of upload data if it contains no data" {
+            val storage = createStorage()
+
+            storage.nextUploadChunk(42) shouldHaveSize 0
+        }
+
+        "OfflineLocationStorage should forbid a direct upload if the store contains elements" {
+            val storage = createStorage()
+
+            storage.storeFailedUpload(locData(1, minTrackingInterval))
+            storage.canUploadDirectly(locData(2, 2 * minTrackingInterval)) shouldBe false
+        }
+
+        "OfflineLocationStorage should return the next chunk of data to be uploaded" {
+            val data1 = locData(1, minTrackingInterval)
+            val data2 = locData(2, 2 * minTrackingInterval)
+            val data3 = locData(3, 3 * minTrackingInterval)
+            val storage = createStorageWithData(locData(0, 0), data1, data2, data3)
+
+            val chunk = storage.nextUploadChunk(3)
+            chunk shouldContainExactly listOf(data3, data2, data1)
+        }
+
+        "OfflineLocationStorage should remove the items returned as the next data chunk" {
+            val data0 = locData(0, 0)
+            val data1 = locData(1, minTrackingInterval)
+            val data2 = locData(2, 2 * minTrackingInterval)
+            val data3 = locData(3, 3 * minTrackingInterval)
+            val storage = createStorageWithData(data0, data1, data2, data3)
+            storage.nextUploadChunk(3)
+
+            val chunk = storage.nextUploadChunk(3)
+            chunk shouldContainExactly listOf(data0)
+        }
+
+        "OfflineLocationStorage should process a successful multi-upload operation if no data is pending" {
+            val locData = listOf(
+                locData(1, minTrackingInterval),
+                locData(2, minTrackingInterval * 2),
+                locData(3, minTrackingInterval * 3)
+            )
+            val storage = createStorage()
+
+            storage.handleMultiUploadResult(locData,
+                locData.size) shouldBe OfflineLocationStorage.Companion.MultiUploadProgress.DONE
+            storage.nextUploadChunk(42) shouldHaveSize 0
+        }
+
+        "OfflineLocationStorage should process a successful multi-upload operation if more data is pending" {
+            val locData = listOf(
+                locData(1, minTrackingInterval),
+                locData(2, minTrackingInterval * 2),
+                locData(3, minTrackingInterval * 3)
+            )
+            val data0 = locData(0, 0)
+            val storage = createStorageWithData(data0)
+
+            storage.handleMultiUploadResult(locData,
+                locData.size) shouldBe OfflineLocationStorage.Companion.MultiUploadProgress.PROGRESS
+            storage.nextUploadChunk(42) shouldContainExactly listOf(data0)
+        }
+
+        "OfflineLocationStorage should process a partly failed multi-upload operation" {
+            val locData = listOf(
+                locData(3, minTrackingInterval * 3),
+                locData(2, minTrackingInterval * 2),
+                locData(1, minTrackingInterval)
+            )
+            val data0 = locData(0, 0)
+            val storage = createStorageWithData(data0)
+
+            storage.handleMultiUploadResult(locData,
+                2) shouldBe OfflineLocationStorage.Companion.MultiUploadProgress.ERROR
+            storage.nextUploadChunk(3) shouldContainExactly listOf(locData[2], data0)
+        }
+
+        "OfflineLocationStore should add the element of a direct upload check to the store if needed" {
+            val data1 = locData(1, minTrackingInterval)
+            val data2 = locData(2, minTrackingInterval * 2)
+            val storage = createStorageWithData(data1)
+
+            storage.canUploadDirectly(data2) shouldBe false
+            storage.nextUploadChunk(3) shouldContainExactly listOf(data2, data1)
+        }
+
+        "OfflineLocationStore should ensure its maximum capacity" {
+            val data = (1..(capacity + 1)).map { idx -> locData(idx, minTrackingInterval * idx) }
+            val storage = createStorageWithData(*data.toTypedArray())
+
+            val chunk = storage.nextUploadChunk(capacity + 2)
+            chunk shouldHaveSize capacity
+            chunk shouldNotContain data[0]
+        }
+
+        "OfflineLocationStore should filter out data arriving with a frequency too high" {
+            val data0 = locData(0, 0)
+            val data1 = locData(1, 10000)
+            val data2 = locData(2, 40000)
+            val data3 = locData(3, minTrackingInterval)
+            val data4 = locData(4, minTrackingInterval + 45000)
+            val data5 = locData(5, 2 * minTrackingInterval - 1)
+            val data6 = locData(6, 3 * minTrackingInterval)
+            val storage = createStorageWithData(data0, data1, data2, data3, data4, data5, data6)
+
+            val content = storage.nextUploadChunk(capacity)
+            content shouldContainExactly listOf(data6, data5, data3, data1, data0)
+        }
+    }
+
+    companion object {
+        /** The capacity of the test storage. */
+        private const val capacity = 8
+
+        /** The minimum tracking interval. */
+        private const val minTrackingInterval = 60000L
+
+        /** The reference latitude to generate GPS data. */
+        private const val refLatitude = 7.12
+
+        /** The reference longitude to generate GPS data. */
+        private const val refLongitude = 12.89
+
+        /**
+         * Time value used as reference time when generating relative
+         * _TimeData_ objects.
+         */
+        private val referenceTime = System.currentTimeMillis()
+
+        /**
+         * Creates a new test instance with default settings.
+         * @return the new test instance
+         */
+        private fun createStorage(): OfflineLocationStorage =
+            OfflineLocationStorage(capacity, minTrackingInterval)
+
+        /**
+         * Creates a new test instance with default settings and adds the
+         * given test data items to it.
+         * @param data the data items to be added
+         * @return the test storage instance
+         */
+        private fun createStorageWithData(vararg data: LocationData): OfflineLocationStorage {
+            val storage = createStorage()
+            data.forEach(storage::storeFailedUpload)
+            return storage
+        }
+
+        /**
+         * Generates a test _LocationData_ object based on the passed in
+         * parameters. From the index, unique GPS position data is generated.
+         * The _deltaT_ parameter is interpreted as an offset in milliseconds
+         * to a reference time.
+         * @param index the index of the data object
+         * @param deltaT the time offset when this data was created
+         * @return the new _LocationData_ object
+         */
+        private fun locData(index: Int, deltaT: Long): LocationData {
+            val posDelta = index.toDouble() / 100.0
+            return LocationData(
+                latitude = refLatitude + posDelta, longitude = refLongitude - posDelta,
+                time = TimeData(referenceTime + deltaT)
+            )
+        }
+    }
+}
