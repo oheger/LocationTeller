fb2cf445aca2382f66440854dfed235f1f7f882a
oheger <oliver.heger@oliver-heger.de>
2021-09-28 21:42:59 +0200

    Updated Copyright dates.

--- app/src/test/java/com/github/oheger/locationteller/ui/TrackStatsFormatterSpec.kt
+++ app/src/test/java/com/github/oheger/locationteller/ui/TrackStatsFormatterSpec.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2020 The Developers.
+ * Copyright 2019-2021 The Developers.
  *
  * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License.
