f6a9a65e211308f8883b299a5f9e6be26dc61b77
oheger <oliver.heger@oliver-heger.de>
2022-06-03 21:27:54 +0200

    Updated copyright dates.

--- server-access/src/main/java/com/github/oheger/locationteller/server/TimeService.kt
+++ server-access/src/main/java/com/github/oheger/locationteller/server/TimeService.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2021 The Developers.
+ * Copyright 2019-2022 The Developers.
  *
  * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License.
