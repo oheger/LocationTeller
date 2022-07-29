d1aa49f520c5c6abcfee2d9d9b9d11ad19ff921f
oheger <oliver.heger@oliver-heger.de>
2022-07-29 20:54:50 +0200

    TrackStatsFormatterSpec: Reworked the test class.
    Switched from StringSpec to WordSpec, since multiple functions are
    tested.
    
    Moved functions from the companion object to top-level functions, used
    more Kotlin-style in general.

--- app/src/test/java/com/github/oheger/locationteller/ui/state/TrackStatsFormatterSpec.kt
+++ app/src/test/java/com/github/oheger/locationteller/ui/state/TrackStatsFormatterSpec.kt
@@ -17,22 +17,37 @@ package com.github.oheger.locationteller.ui.state
 
 import com.github.oheger.locationteller.server.TimeData
 import com.github.oheger.locationteller.server.TimeService
-import io.kotest.core.spec.style.StringSpec
+
+import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.longs.shouldBeLessThanOrEqual
 import io.kotest.matchers.shouldBe
+
 import io.mockk.every
 import io.mockk.mockk
+
 import java.text.DateFormat
 import java.text.NumberFormat
-import java.util.*
+import java.util.Calendar
+import java.util.Date
+
 import kotlin.math.abs
 
 /**
  * Test class for [[TrackStatsFormatter]].
  */
-class TrackStatsFormatterSpec : StringSpec() {
-    init {
-        "TrackStatsFormatter should correctly format a number" {
+class TrackStatsFormatterSpec : WordSpec({
+    "create" should {
+        "create a correct time service" {
+            val formatter = TrackStatsFormatter.create()
+
+            val currentTime = formatter.timeService.currentTime().currentTime
+            val deltaT = abs(System.currentTimeMillis() - currentTime)
+            deltaT shouldBeLessThanOrEqual 3000
+        }
+    }
+
+    "formatNumber" should {
+        "correctly format a number" {
             val value = 3.1415
             val format = NumberFormat.getInstance()
             format.minimumFractionDigits = 2
@@ -42,128 +57,111 @@ class TrackStatsFormatterSpec : StringSpec() {
 
             formatter.formatNumber(value) shouldBe expectedResult
         }
+    }
 
-        "TrackStatsFormatter should format a duration in seconds" {
+    "formatDuration" should {
+        "format a duration in seconds" {
             val formatter = TrackStatsFormatter.create()
 
             formatter.formatDuration(9000L) shouldBe "0:09"
         }
 
-        "TrackStatsFormatter should format a duration in minutes" {
+        "format a duration in minutes" {
             val formatter = TrackStatsFormatter.create()
 
             formatter.formatDuration(612000L) shouldBe "10:12"
         }
 
-        "TrackStatsFormatter should format a duration in hours" {
+        "format a duration in hours" {
             val formatter = TrackStatsFormatter.create()
 
             formatter.formatDuration(7800000L) shouldBe "2:10:00"
         }
 
-        "TrackStatsFormatter should format a duration in days" {
+        "format a duration in days" {
             val formatter = TrackStatsFormatter.create()
 
             formatter.formatDuration(1036802000L) shouldBe "12:00:00:02"
         }
 
-        "TrackingStatsFormatter should a zero duration" {
+        "format a zero duration" {
             val formatter = TrackStatsFormatter.create()
 
             formatter.formatDuration(0L) shouldBe "0:00"
         }
+    }
 
-        "TrackingStatsFormatter should create a correct time service" {
-            val formatter = TrackStatsFormatter.create()
-
-            val currentTime = formatter.timeService.currentTime().currentTime
-            val deltaT = abs(System.currentTimeMillis() - currentTime)
-            deltaT shouldBeLessThanOrEqual 3000
-        }
-
-        "TrackingStatsFormatter should format a date on the same day" {
+    "formatDate" should {
+        "format a date on the same day" {
             val date = toDate(22, 1, 12)
             val now = toDate(22, 2, 48)
 
             checkFormatDate(date, now, dateString(date, withDatePortion = false))
         }
 
-        "TrackingStatsFormatter should format a date on another day" {
+        "format a date on another day" {
             val date = toDate(22, 1, 12, day = 5)
             val now = toDate(22, 18, 12)
 
             checkFormatDate(date, now, dateString(date, withDatePortion = true))
         }
 
-        "TrackingStatsFormatter should format a date on another month" {
+        "format a date on another month" {
             val date = toDate(21, 33, 47, month = Calendar.MARCH)
             val now = toDate(21, 33, 47)
 
             checkFormatDate(date, now, dateString(date, withDatePortion = true))
         }
 
-        "TrackingStatsFormatter should format a date on another year" {
+        "format a date on another year" {
             val date = toDate(21, 39, 55, year = 2019)
             val now = toDate(21, 39, 55)
 
             checkFormatDate(date, now, dateString(date, withDatePortion = true))
         }
 
-        "TrackingStatsFormatter should format a null date" {
+        "format a null date" {
             val formatter = TrackStatsFormatter.create()
 
             formatter.formatDate(null) shouldBe ""
         }
     }
+})
 
-    companion object {
-        /**
-         * Generates a date object with the given time portion and an optional
-         * date portion.
-         * @param hour the hour of the day
-         * @param minute the minute
-         * @param second the second
-         * @param year the year
-         * @param month the month
-         * @param day the day of month
-         * @return the resulting date
-         */
-        private fun toDate(
-            hour: Int, minute: Int, second: Int, year: Int = 2020,
-            month: Int = Calendar.FEBRUARY, day: Int = 6
-        ): Date =
-            Calendar.getInstance().run {
-                set(year, month, day, hour, minute, second)
-                time
-            }
-
-        /**
-         * Helper function to check whether a date is formatted correctly.
-         * @param date the date to format
-         * @param now the current time
-         * @param expected the expected string result
-         */
-        private fun checkFormatDate(date: Date, now: Date, expected: String) {
-            val timeService = mockk<TimeService>()
-            every { timeService.currentTime() } returns TimeData(now.time)
-            val formatter = TrackStatsFormatter.create(timeService)
-
-            val result = formatter.formatDate(date)
-            result shouldBe expected
-        }
-
-        /**
-         * Generates the formatted string to be displayed for a date based on
-         * the given criteria.
-         * @param date the date
-         * @param withDatePortion flag whether the date part should be
-         * contained
-         * @return the formatted date string
-         */
-        private fun dateString(date: Date, withDatePortion: Boolean): String {
-            val timeStr = DateFormat.getTimeInstance(DateFormat.MEDIUM).format(date)
-            return timeStr.takeUnless { withDatePortion }
-                ?: "${DateFormat.getDateInstance(DateFormat.MEDIUM).format(date)} $timeStr"
-        }
+/**
+ * Generate a [Date] object from the given parameters.
+ */
+private fun toDate(
+    hour: Int,
+    minute: Int,
+    second: Int,
+    year: Int = 2020,
+    month: Int = Calendar.FEBRUARY,
+    day: Int = 6
+): Date =
+    Calendar.getInstance().run {
+        set(year, month, day, hour, minute, second)
+        time
     }
+
+/**
+ * Check whether a [date] is correctly formatted to [expected] given the [current time][now].
+ */
+private fun checkFormatDate(date: Date, now: Date, expected: String) {
+    val timeService = mockk<TimeService>()
+    every { timeService.currentTime() } returns TimeData(now.time)
+    val formatter = TrackStatsFormatter.create(timeService)
+
+    val result = formatter.formatDate(date)
+    result shouldBe expected
+}
+
+/**
+ * Generate the formatted string to be displayed for [date] based on a [flag][withDatePortion] whether the date
+ * portion should be included.
+ */
+private fun dateString(date: Date, withDatePortion: Boolean): String {
+    val timeStr = DateFormat.getTimeInstance(DateFormat.MEDIUM).format(date)
+    return timeStr.takeUnless { withDatePortion }
+        ?: "${DateFormat.getDateInstance(DateFormat.MEDIUM).format(date)} $timeStr"
 }
